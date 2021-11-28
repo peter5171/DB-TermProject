194b22590a5d4a99796f2947826edcd9b79da8fc
pangkin <git@pangkin.com>
2021-11-28 21:39:02 +0900

    maybe finish

--- routes/admin.js
+++ routes/admin.js
@@ -1,77 +1,78 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import passport from "passport";
-import { ensureLoggedOut } from "connect-ensure-login";
+import express from 'express';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import { ensureLoggedOut } from 'connect-ensure-login';
 
-import { runInsert, runSelect } from "../lib/db";
+import { runInsert, runSelect } from '../lib/db';
 
 var router = express.Router();
 
-router.get("/", async (req, res) => {
-  const users = await runSelect("SELECT 아이디, 회원이름, 등급 FROM 회원", []);
+router.get('/', async (req, res) => {
+  const users = await runSelect('SELECT 아이디, 회원이름, 등급 FROM 회원', []);
 
-  res.render("admin", { title: "Express", users });
+  res.render('admin', { title: 'Express', users });
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const { username, rank } = req.body;
 
-  runInsert("UPDATE 회원 SET 등급 = :1 WHERE 아이디 = :2", [rank, username]);
-  res.redirect("/admin");
+  runInsert('UPDATE 회원 SET 등급 = :1 WHERE 아이디 = :2', [rank, username]);
+  res.redirect('/admin');
 });
 
-router.get("/movie", async (req, res) => {
+router.get('/movie', async (req, res) => {
   const movies = await runSelect(
-    "SELECT 영화번호, 영화제목, 영화감독, 주연배우,포스터 FROM 영화",
+    'SELECT 영화번호, 영화제목, 영화감독, 주연배우,포스터 FROM 영화',
     []
   );
-  res.render("movie", { title: "Express", movies });
+  res.render('movie', { title: 'Express', movies });
 });
 
-router.post("/movie", async (req, res) => {
-  res.render("movie", { title: "Express" });
+router.post('/movie', async (req, res) => {
+  res.render('movie', { title: 'Express' });
 });
 
-router.get("/addmovie", (req, res) => {
-  res.render("addmovie", { title: "Express" });
+router.get('/addmovie', (req, res) => {
+  res.render('addmovie', { title: 'Express' });
 });
 
-router.post("/addmovie", async (req, res) => {
+router.post('/addmovie', async (req, res) => {
   const { title, director, actor } = req.body;
-  console.log(title, director, actor, req.files.poster.data);
+  console.log(title, director, actor, req.files.poster);
   runInsert(
-    "INSERT INTO 영화 VALUES (AUTO_INCREMENT_MOVIE.nextval, :1, :2, :3, :4)",
+    'INSERT INTO 영화 VALUES (AUTO_INCREMENT_MOVIE.nextval, :1, :2, :3, :4)',
     [title, director, actor, req.files.poster.data]
   );
-  res.redirect("/admin/movie");
+  res.redirect('/admin/movie');
 });
 
-router.get("/schedule", async (req, res) => {
+router.get('/schedule', async (req, res) => {
   const schedules = await runSelect(
-    "SELECT 영화번호, 상영시간, 상영관번호, 요금 FROM 상영스케줄",
+    'SELECT 영화번호, 상영시간, 상영관번호, 요금 FROM 상영스케줄',
     []
   );
-  res.render("schedule", { title: "Express", schedules });
+  res.render('schedule', { title: 'Express', schedules });
 });
 
-router.post("/schedule", async (req, res) => {
-  res.render("schedule", { title: "Express" });
+router.post('/schedule', async (req, res) => {
+  res.render('schedule', { title: 'Express' });
 });
 
-router.get("/addschedule", (req, res) => {
-  res.render("addschedule", { title: "Express" });
+router.get('/addschedule/:id', (req, res) => {
+  const { id } = req.params;
+  res.render('addschedule', { title: 'Express', id });
 });
 
-router.post("/addschedule", async (req, res) => {
-  const movienum = runSelect(
-    "SELECT 상영스케줄.영화번호 AS 영화번호 FORM 상영스케줄 JOIN 영화 ON 영화.영화번호 = 상영스케줄.영화번호 WHERE teatime=:1, teanum=:2",
-    [상영스케줄.상영시간, 상영스케줄.상영관번호]
+router.post('/addschedule/:id', async (req, res) => {
+  const { id } = req.params;
+  const { mvdate, mvtime, teanum, cost } = req.body;
+  const date = new Date(`${mvdate} ${mvtime}:00`);
+
+  console.log(parseInt(cost));
+
+  runInsert(
+    'INSERT INTO 상영스케줄 (영화번호, 상영시간, 상영관번호, 요금) VALUES (:1, :2, :3, :4)',
+    [id, date, teanum, parseInt(cost)]
   );
-  const { movienum, mvtime, teanum, cost } = req.body;
-  runInsert("INSERT INTO 상영스케줄 VALUES (movienum, :1, :2, :3)", [
-    mvtime,
-    teanum,
-    cost,
-  ]);
 });
 export default router;
--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,77 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import passport from "passport";
-import { ensureLoggedIn, ensureLoggedOut } from "connect-ensure-login";
+import express from 'express';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import { ensureLoggedIn, ensureLoggedOut } from 'connect-ensure-login';
 
-import { runInsert, runSelect } from "../lib/db";
+import { getImage, runInsert, runSelect } from '../lib/db';
+import OracleDB from 'oracledb';
 
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res) => {
-  res.render("index", {
-    title: "Express",
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Express',
     loggedIn: req.user ? true : false,
   });
 });
 
-router.get("/signin", ensureLoggedOut({ redirectTo: "/" }), (req, res) => {
-  res.render("signin", { title: "Express" });
+router.get('/signin', ensureLoggedOut({ redirectTo: '/' }), (req, res) => {
+  res.render('signin', { title: 'Express' });
 });
 
 router.post(
-  "/signin",
-  ensureLoggedOut({ redirectTo: "/" }),
+  '/signin',
+  ensureLoggedOut({ redirectTo: '/' }),
   async (req, res) => {
     const { name, username, password, tel, card } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    runInsert("INSERT INTO 회원 VALUES (:1, :2, :3, DEFAULT, :4, :5)", [
+    runInsert('INSERT INTO 회원 VALUES (:1, :2, :3, DEFAULT, :4, :5)', [
       username,
       name,
       tel,
       card,
       hashedPassword,
     ]);
-    res.redirect("/login");
+    res.redirect('/login');
   }
 );
 
-router.get("/login", ensureLoggedOut({ redirectTo: "/" }), (req, res) => {
-  res.render("login", {
-    title: "Express",
+router.get('/login', ensureLoggedOut({ redirectTo: '/' }), (req, res) => {
+  res.render('login', {
+    title: 'Express',
   });
 });
 
 router.post(
-  "/login",
+  '/login',
   //로그아웃 상태 확인
-  ensureLoggedOut({ redirectTo: "/" }),
+  ensureLoggedOut({ redirectTo: '/' }),
   //로그인 하는 부분
-  passport.authenticate("local", {
-    successReturnToOrRedirect: "/",
-    failureRedirect: "/login",
+  passport.authenticate('local', {
+    successReturnToOrRedirect: '/',
+    failureRedirect: '/login',
   })
 );
 
 router.get(
-  "/logout",
-  ensureLoggedIn({ setReturnTo: true, redirectTo: "/login" }),
+  '/logout',
+  ensureLoggedIn({ setReturnTo: true, redirectTo: '/login' }),
   (req, res) => {
     req.session.destroy((err) => {
       if (err) throw err;
-      res.redirect("/");
+      res.redirect('/');
     });
   }
 );
 
 router.get(
-  "/mypage",
-  ensureLoggedIn({ setReturnTo: true, redirectTo: "/login" }),
+  '/mypage',
+  ensureLoggedIn({ setReturnTo: true, redirectTo: '/login' }),
   (req, res) => {
-    res.render("mypage", {
-      title: "Express",
+    res.render('mypage', {
+      title: 'Express',
       user: req.user,
       loggedIn: req.user ? true : false,
     });
@@ -78,33 +79,33 @@ router.get(
 );
 
 router.post(
-  "/mypage",
-  ensureLoggedIn({ setReturnTo: true, redirectTo: "/login" }),
+  '/mypage',
+  ensureLoggedIn({ setReturnTo: true, redirectTo: '/login' }),
   async (req, res) => {
     const { username, password, name, tel, card } = req.body;
 
     let sql,
       data = [];
-    if (password && password !== "") {
+    if (password && password !== '') {
       const hashedPassword = await bcrypt.hash(password, 10);
       sql =
-        "UPDATE 회원 SET 비밀번호 = :1, 회원이름 = :2, 휴대전화 = :3, 카드번호 = :4 WHERE 아이디 = :5";
+        'UPDATE 회원 SET 비밀번호 = :1, 회원이름 = :2, 휴대전화 = :3, 카드번호 = :4 WHERE 아이디 = :5';
       data.push(hashedPassword, name, tel, card, username);
     } else {
       sql =
-        "UPDATE 회원 SET 회원이름 = :1, 휴대전화 = :2, 카드번호 = :3 WHERE 아이디 = :4";
+        'UPDATE 회원 SET 회원이름 = :1, 휴대전화 = :2, 카드번호 = :3 WHERE 아이디 = :4';
       data.push(name, tel, card, username);
     }
     runInsert(sql, data);
-    res.redirect("/mypage");
+    res.redirect('/mypage');
   }
 );
 
 router.post(
-  "/mypage/delete",
-  ensureLoggedIn({ setReturnTo: true, redirectTo: "/login" }),
+  '/mypage/delete',
+  ensureLoggedIn({ setReturnTo: true, redirectTo: '/login' }),
   async (req, res) => {
-    runInsert("DELETE FROM 회원 WHERE 아이디 = :1", [req.user.아이디]);
+    runInsert('DELETE FROM 회원 WHERE 아이디 = :1', [req.user.아이디]);
     req.session.destroy((err) => {
       if (err) throw err;
       res.json({ success: true });
@@ -112,28 +113,39 @@ router.post(
   }
 );
 
-router.get("/viewmv", async (req, res) => {
+router.get('/viewmv', async (req, res) => {
   const movies = await runSelect(
-    "SELECT 영화번호, 영화제목, 영화감독, 주연배우,포스터 FROM 영화",
+    'SELECT 영화번호, 영화제목, 영화감독, 주연배우,포스터 FROM 영화',
     []
   );
-  res.render("viewmv", { title: "Express", movies });
+  res.render('viewmv', { title: 'Express', movies });
 });
 
-router.post("/viewmv", (req, res) => {
-  res.render("viewmv", { title: "Express" });
+router.post('/viewmv', (req, res) => {
+  res.render('viewmv', { title: 'Express' });
 });
 
-router.get("/viewschedule", async (req, res) => {
+router.get('/viewschedule', async (req, res) => {
   const schedules = await runSelect(
-    "SELECT 영화번호, 상영시간, 상영관번호, 요금 FROM 상영스케줄",
+    'SELECT 영화번호, 상영시간, 상영관번호, 요금 FROM 상영스케줄',
     []
   );
 
-  res.render("viewschedule", { title: "Express", schedules });
+  res.render('viewschedule', { title: 'Express', schedules });
 });
 
-router.post("/viewschedule", (req, res) => {
-  res.render("viewschedule", { title: "Express" });
+router.post('/viewschedule', (req, res) => {
+  res.render('viewschedule', { title: 'Express' });
+});
+
+router.get('/poster/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const poster = await getImage('SELECT 포스터 FROM 영화 WHERE 영화번호 = :1', [
+    id,
+  ]);
+  console.log(poster[0][0]);
+
+  res.send(poster[0][0]);
 });
 export default router;
