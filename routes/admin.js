3f94a102e55d757044741d6d7222255c63c99374
pangkin <git@pangkin.com>
2021-11-28 14:49:18 +0900

    mypage

--- routes/admin.js
+++ routes/admin.js
@@ -1,53 +1,53 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import passport from "passport";
-import { ensureLoggedOut } from "connect-ensure-login";
+import express from 'express';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import { ensureLoggedOut } from 'connect-ensure-login';
 
-import { runInsert } from "../lib/user";
+import { runInsert } from '../lib/user';
 
 var router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("admin", { title: "Express" });
+router.get('/', (req, res) => {
+  res.render('admin', { title: 'Express' });
 });
 
-router.get("/movie", (req, res) => {
-  res.render("movie", { title: "Express" });
+router.get('/movie', (req, res) => {
+  res.render('movie', { title: 'Express' });
 });
 
-router.post("/movie", async (req, res) => {
-  res.render("movie", { title: "Express" });
+router.post('/movie', async (req, res) => {
+  res.render('movie', { title: 'Express' });
 });
 
-router.get("/addmovie", (req, res) => {
-  res.render("addmovie", { title: "Express" });
+router.get('/addmovie', (req, res) => {
+  res.render('addmovie', { title: 'Express' });
 });
 
-router.post("/addmovie", async (req, res) => {
+router.post('/addmovie', async (req, res) => {
   const { poster, mvnum, mvtitle, mvpd, actor } = req.body;
 
   runInsert(
-    "INSERT INTO 영화 VALUES (:1,AUTO_INCRSEMENT_MOVIE.nextvalue , :2, :3, :4)",
+    'INSERT INTO 영화 VALUES (:1,AUTO_INCRSEMENT_MOVIE.nextvalue , :2, :3, :4)',
     [poster, mvtitle, mvpd, actor]
   );
 });
 
-router.get("/shcedule", (req, res) => {
-  res.render("shcedule", { title: "Express" });
+router.get('/schedule', (req, res) => {
+  res.render('schedule', { title: 'Express' });
 });
 
-router.post("/shcedule", async (req, res) => {
-  res.render("shcedule", { title: "Express" });
+router.post('/schedule', async (req, res) => {
+  res.render('schedule', { title: 'Express' });
 });
 
-router.get("/addshcedule", (req, res) => {
-  res.render("addshcedule", { title: "Express" });
+router.get('/addschedule', (req, res) => {
+  res.render('addschedule', { title: 'Express' });
 });
 
-router.post("/addshcedule", async (req, res) => {
+router.post('/addschedule', async (req, res) => {
   const { poster, mvnum, mvtitle, mvpd, actor } = req.body;
 
-  runInsert("INSERT INTO 상영시간 VALUES (:1, :2, :3, :4)", [
+  runInsert('INSERT INTO 상영시간 VALUES (:1, :2, :3, :4)', [
     mvnum,
     teatime,
     teanum,
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,34 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import passport from "passport";
-import { ensureLoggedOut } from "connect-ensure-login";
+import express from 'express';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import { ensureLoggedOut } from 'connect-ensure-login';
 
-import { runInsert } from "../lib/user";
+import { runInsert } from '../lib/user';
 
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res) => {
-  res.render("index", {
-    title: "Express",
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Express',
     userInfo: {
-      id: "asff",
+      id: 'asff',
     },
   });
 });
 
-router.get("/signin", ensureLoggedOut({ redirectTo: "/" }), (req, res) => {
-  res.render("signin", { title: "Express" });
+router.get('/signin', ensureLoggedOut({ redirectTo: '/' }), (req, res) => {
+  res.render('signin', { title: 'Express' });
 });
 
 router.post(
-  "/signin",
-  ensureLoggedOut({ redirectTo: "/" }),
+  '/signin',
+  ensureLoggedOut({ redirectTo: '/' }),
   async (req, res) => {
     const { name, username, password, tel, card } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    runInsert("INSERT INTO 회원 VALUES (:1, :2, :3, DEFAULT, :4, :5)", [
+    runInsert('INSERT INTO 회원 VALUES (:1, :2, :3, DEFAULT, :4, :5)', [
       username,
       name,
       tel,
@@ -38,21 +38,31 @@ router.post(
   }
 );
 
-router.get("/login", ensureLoggedOut({ redirectTo: "/" }), (req, res) => {
-  res.render("login", {
-    title: "Express",
+router.get('/login', ensureLoggedOut({ redirectTo: '/' }), (req, res) => {
+  res.render('login', {
+    title: 'Express',
   });
 });
 
 router.post(
-  "/login",
+  '/login',
   //로그아웃 상태 확인
-  ensureLoggedOut({ redirectTo: "/" }),
+  ensureLoggedOut({ redirectTo: '/' }),
   //로그인 하는 부분
-  passport.authenticate("local", {
-    successReturnToOrRedirect: "/",
-    failureRedirect: "/login",
+  passport.authenticate('local', {
+    successReturnToOrRedirect: '/',
+    failureRedirect: '/login',
   })
+);
+
+router.get(
+  '/mypage',
+  /*ensureLoggedOut({ redirectTo: '/' }),*/
+  (req, res) => {
+    res.render('mypage', {
+      title: 'Express',
+    });
+  }
 );
 
 export default router;
